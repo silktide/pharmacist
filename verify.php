482fcb49ac1c8769621b18ca2ad8e521bb90e5fe
Doug <dougnelson@silktide.com>
2016-04-13 15:36:46 +0100

    added dealing with autoloader in a way that allows use in require-dev

--- verify.php
+++ verify.php
@@ -1,7 +1,12 @@
 <?php
 
-include(__DIR__."/vendor/autoload.php");
-
+$baseAutoloader = __DIR__."/vendor/autoload.php";
+if (file_exists($baseAutoloader)) {
+    require_once($baseAutoloader);
+} else {
+    // Otherwise, we're probably included via require-dev
+    require_once("vendor/autoload.php");
+}
 
 $application = new \Symfony\Component\Console\Application("SyringeVerifier", "1.0");
 $application->add(new \Silktide\SyringeVerifier\VerifyCommand());
